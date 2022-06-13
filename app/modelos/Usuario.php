93a9e935f837dee812e4e23b075e043f9173e383
cristiansaezpracticas <cristiansaezpracticas@gmail.com>
2022-06-13 16:59:00 +0200

    notapp: modelos: Creación de la clase Usuario

---
+++ app/modelos/Usuario.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Description of Usuario
+ *
+ * @author Cristian
+ */
+ 
+class Usuario {
+	
+    private $id;
+    private $nombre;
+    private $email;
+    private $password;
+    private $foto;
+    private $cookie_id;
+	
+	//Array que contendrá las notas del usuario
+    private $notas;
+    
+
+    function getCookie_id() {
+        return $this->cookie_id;
+    }
+
+    function setCookie_id($cookie_id): void {
+        $this->cookie_id = $cookie_id;
+    }
+
+    function getId() {
+        return $this->id;
+    }
+
+    function getNombre() {
+        return $this->nombre;
+    }
+
+    function getEmail() {
+        return $this->email;
+    }
+
+    function getPassword() {
+        return $this->password;
+    }
+
+    function getFoto() {
+        return $this->foto;
+    }
+
+    function getNotas() {
+        return $this->notas;
+    }
+
+    function setId($id): void {
+        $this->id = $id;
+    }
+
+    function setNombre($nombre): void {
+        $this->nombre = $nombre;
+    }
+
+    function setEmail($email): void {
+        $this->email = $email;
+    }
+
+    function setPassword($password): void {
+        $this->password = $password;
+    }
+
+    function setFoto($foto): void {
+        $this->foto = $foto;
+    }
+
+    function setNotas($notas): void {
+        $this->notas = $notas;
+    }
+
+}
